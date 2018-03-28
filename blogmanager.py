36ef346d4676197924055cd44d8fc269c9630ed3
cngoodboy <cngoodboy@qq.com>
2018-03-28 03:53:47 -0500

    Add files via upload

---
+++ blogmanager.py
@@ -0,0 +1,27 @@
+import os
+import json
+import io
+
+
+blog_info = dict(langth = 0, value=list())
+
+blogs = os.listdir("./blog")
+
+blogs = list(filter(lambda x: x.endswith(".md"), blogs))
+
+blogs.sort()
+
+blog_list = []
+
+for i in blogs:
+    blog_dict = {}
+    with open("./blog/" + i, encoding="utf-8") as f:
+        blog_dict['title'] = f.readline()[2:-1]
+    blog_dict['href'] = "./blog/" + i
+    blog_info['value'].append(blog_dict)
+
+blog_info['value'].reverse()
+
+blog_info['length'] = len(blog_info['value'])
+
+json.dump(blog_info, open("blog/bloglist.json", 'w', encoding="utf-8"), ensure_ascii=False)
