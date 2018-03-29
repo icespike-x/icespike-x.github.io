06daab37105cfa47202c5048448c57ae34a2a7c2
cngoodboy <cngoodboy@qq.com>
2018-03-29 12:59:56 +0800

    Format timestamp.py

--- timestamp.py
+++ timestamp.py
@@ -10,5 +10,5 @@ weekday = ["一", "二", "三", "四", "五", "六", "日"]
 with open(sys.argv[1], 'w', encoding="utf-8") as f:
     f.write(orignal[0])
     currentTime = time.localtime()
-    f.write("%d/%d/%d 星期%s %d:%d\n" % (currentTime.tm_year, currentTime.tm_mon, currentTime.tm_mday, weekday[currentTime.tm_wday],currentTime.tm_hour, currentTime.tm_min))
+    f.write("%d/%d/%d 星期%s %d:%d\n" % (currentTime.tm_year, currentTime.tm_mon, currentTime.tm_mday, weekday[currentTime.tm_wday], currentTime.tm_hour, currentTime.tm_min))
     f.write(''.join(orignal[1:]))
