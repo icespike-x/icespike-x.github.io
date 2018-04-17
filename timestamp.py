9283f3ad3addba4954e3d249d427020a390dbb6f
cngoodboy <cngoodboy@qq.com>
2018-04-17 12:05:52 +0800

    Update timestamp.py

--- timestamp.py
+++ timestamp.py
@@ -10,6 +10,6 @@ weekday = ["一", "二", "三", "四", "五", "六", "日"]
 with open(os.path.join("./blog/", sys.argv[1]), 'w', encoding="utf-8") as f:
     f.write(orignal[0])
     currentTime = time.localtime()
-    f.write("%d/%d/%d 星期%s %d:%d\n" % (currentTime.tm_year, currentTime.tm_mon, currentTime.tm_mday, weekday[currentTime.tm_wday], currentTime.tm_hour, currentTime.tm_min))
+    f.write("%d/%d/%d 星期%s %2d:%2d\n" % (currentTime.tm_year, currentTime.tm_mon, currentTime.tm_mday, weekday[currentTime.tm_wday], currentTime.tm_hour, currentTime.tm_min))
     f.write("<font style=\"color: red; font-size: 16pt\">本文章为作者原创文章，未经作者允许严禁转载！</font>")
     f.write(''.join(orignal[1:]))
