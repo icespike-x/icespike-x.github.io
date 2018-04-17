3347ef885aa0a784e77c10fdbe6b890178fa9900
cngoodboy <cngoodboy@qq.com>
2018-04-17 13:40:47 +0800

    Add new blog

--- timestamp.py
+++ timestamp.py
@@ -11,5 +11,5 @@ with open(os.path.join("./blog/", sys.argv[1]), 'w', encoding="utf-8") as f:
     f.write(orignal[0])
     currentTime = time.localtime()
     f.write("%d/%d/%d 星期%s %2d:%2d\n" % (currentTime.tm_year, currentTime.tm_mon, currentTime.tm_mday, weekday[currentTime.tm_wday], currentTime.tm_hour, currentTime.tm_min))
-    f.write("<font style=\"color: red; font-size: 16pt\">本文章为作者原创文章，未经作者允许严禁转载！</font>")
+    f.write("<font style=\"color: red; font-size: 16pt\">本文章为作者原创文章，未经作者允许严禁转载！</font>\n")
     f.write(''.join(orignal[1:]))
